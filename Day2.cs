5d1804f2fef1b9c78fe75689da3915f3611d6511
bfullwood <111522513+bfullwood@users.noreply.github.com>
2022-09-27 15:14:47 -0400

    Add files via upload

---
+++ Day2.cs
@@ -0,0 +1,44 @@
+﻿
+
+Console.WriteLine("Please enter in a number between 1 and 100");
+
+
+string number = Console.ReadLine();
+int num = Int32.Parse(number);
+
+
+
+if (num > 60 && num % 2 != 0) 
+{
+   
+    Console.WriteLine(" “Odd and greater than 60.");
+   
+}
+
+{
+  if  (num < 60 && num % 2 != 0)
+    
+    Console.WriteLine("odd and less than 60");
+}
+
+
+  if  (num >= 2 && num <= 24 && num % 2 == 0)
+{
+
+    Console.WriteLine("even and less than 25");
+}
+
+ if (num >= 26 && num <= 60 && num % 2 == 0) 
+{
+    Console.WriteLine("Even and between 26 and 60");
+}
+
+
+if (num > 60 && num % 2 == 0) 
+{
+    
+    Console.WriteLine(" “Even and greater than 60.");
+}
+for {
+    Console.WriteLine("Please enter in a number between 1 and 100");
+}
